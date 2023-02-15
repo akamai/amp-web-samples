41cd4895c5057295aa1d19738672d4f52848a529
German Murillo <gmurillo@akamai.com>
2023-02-15 15:42:00 -0600

    fixes for node 18

--- react-amp-web/src/App.test.js
+++ react-amp-web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello React+AMP! link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello React\+AMP!/i);
   expect(linkElement).toBeInTheDocument();
 });
